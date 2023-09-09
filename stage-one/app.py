b1f002828e85f9dfda97c4f05943778fcbcc7f34
TessyJames28 <tessyjames28@gmail.com>
2023-09-09 18:19:29 +0100

    Endpoint that takes two GET request parameters

--- stage-one/app.py
+++ stage-one/app.py
@@ -1,16 +1,17 @@
 """A simple app that displays a json response"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from datetime import datetime
 
 app = Flask(__name__)
 
-app.route('/api/')
+@app.route('/api', strict_slashes=False)
 def json_response():
     slack_name = request.args.get('slack_name')
     track = request.args.get('track')
     curr_datetime = datetime.utcnow()
     day = curr_datetime.strftime('%A')
-    date_time = curr_datetime.strftime('%Y%m%dT%H%M%SZ')
+    date_time = curr_datetime.strftime('%Y-%m-%dT%H:%M:%SZ')
+    data = {}
     
     if slack_name is not None and track is not None:
         data = {
@@ -18,9 +19,9 @@ def json_response():
             "current_day": day,
             "utc_time": date_time,
             "track": "backend",
-            "github_file_url": "",
-            "github_repo_url":,
-            "status_code":,
+            "github_file_url": "https://github.com/TessyJames28/HNG/blob/main/stage-one/app.py",
+            "github_repo_url": "https://github.com/TessyJames28/HNG",
+            "status_code": 200,
         }
         
     return jsonify(data)
@@ -28,4 +29,3 @@ def json_response():
 
 if __name__ == "__main__":
     app.run()
-        
